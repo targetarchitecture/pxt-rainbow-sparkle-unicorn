7e0dab52e4ab641ca183d9c2fc7741068afb14b7
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2020-11-24 23:45:59 +0000

    Update main.blocks, main.ts, custom.ts

--- custom.ts
+++ custom.ts
@@ -12,7 +12,31 @@ enum Servo {
     //% block="Servo 2"
     S2 = 2,
     //% block="Servo 3"
-    S3 = 3
+    S3 = 3,
+    //% block="Servo 4"
+    S4 = 4,
+    //% block="Servo 5"
+    S5 = 5,
+    //% block="Servo 6"
+    S6 = 6,
+    //% block="Servo 7"
+    S7 = 7,
+    //% block="Servo 8"
+    S8 = 8,
+    //% block="Servo 9"
+    S9 = 9,
+    //% block="Servo 10"
+    S10 = 10,
+    //% block="Servo 11"
+    S11 = 11,
+    //% block="Servo 12"
+    S12 = 12,
+    //% block="Servo 13"
+    S13 = 13,
+    //% block="Servo 14"
+    S14 = 14,
+    //% block="Servo 15"
+    S15 = 15
 }
 
 const enum TouchSensor {
--- main.ts
+++ main.ts
@@ -2,13 +2,16 @@ input.onButtonPressed(Button.A, function () {
     ADC1voltage += -1
     ADC1voltage = Math.clamp( 0, 30,ADC1voltage)
 RainbowSparkleUnicorn.ADC1(ADC1voltage)
-
+    PWM = randint(100, 505)
+    RainbowSparkleUnicorn.setPulse(Servo.S0, PWM)
+    basic.showNumber(PWM)
 })
 input.onButtonPressed(Button.B, function () {
     ADC1voltage += 1
     ADC1voltage = Math.clamp( 0, 30,ADC1voltage)
 RainbowSparkleUnicorn.ADC1(ADC1voltage)
 })
+let PWM = 0
 let ADC1voltage = 0
 RainbowSparkleUnicorn.start("SN4")
 ADC1voltage = 15
