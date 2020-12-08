0336c337e1996b3f67cf410d54a8434ee3500fe0
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2020-12-08 21:32:47 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,1 @@
-function playSound () {
-    track = randint(1, 60)
-    RainbowSparkleUnicorn.playTrack(track)
-}
-RainbowSparkleUnicorn.onButtonTouched(RainbowSparkleUnicorn.TouchSensor.T8, function () {
-    playSound()
-})
-let track = 0
-RainbowSparkleUnicorn.start()
-RainbowSparkleUnicorn.setVolume(20)
-playSound()
-basic.showIcon(IconNames.Ghost)
+
