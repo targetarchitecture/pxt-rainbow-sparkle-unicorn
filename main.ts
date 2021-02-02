f71d42f79a8a95ae0d305131fc44bfb8af0a76bc
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2021-02-02 16:59:52 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,18 @@ RainbowSparkleUnicorn.Touch.onButtonTouched(touchPins.P1, function () {
 RainbowSparkleUnicorn.Touch.onButtonReleased(touchPins.P1, function () {
     led.unplot(2, 2)
 })
+RainbowSparkleUnicorn.Touch.onButtonTouched(touchPins.P0, function () {
+    led.plot(0, 0)
+})
+RainbowSparkleUnicorn.Touch.onButtonReleased(touchPins.P0, function () {
+    led.unplot(0, 0)
+})
+RainbowSparkleUnicorn.Touch.onButtonTouched(touchPins.P9, function () {
+    led.plot(4, 4)
+})
+RainbowSparkleUnicorn.Touch.onButtonReleased(touchPins.P9, function () {
+    led.unplot(4, 4)
+})
 RainbowSparkleUnicorn.start()
 RainbowSparkleUnicorn.printDebugMessages()
 RainbowSparkleUnicorn.printReceivedMessages()
