ed7ddba3f0a298c530883189e05a8fe305e51ea1
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2021-02-16 00:24:07 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,1 @@
-RainbowSparkleUnicorn.start()
-RainbowSparkleUnicorn.printReceivedMessages()
-RainbowSparkleUnicorn.printDebugMessages()
-let keyA = "qdDjMxAz"
-let keyB = "LvQPRk6u"
-RainbowSparkleUnicorn.IoT.connectToInterWeb(
-"152 2.4GHz",
-"derwenthorpe",
-"targetarchitecture.cloud.shiftr.io",
-"targetarchitecture",
-"" + keyA + keyB,
-"Spaceship Console"
-)
-music.playTone(262, music.beat(BeatFraction.Whole))
+
