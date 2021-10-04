324608cc05b9f47ead639ad86abf9288e65deeb1
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2021-10-04 15:30:56 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,19 @@
-let spinner1 = 0
-let spinner2 = 0
-let slider1 = 0
-let slider2 = 0
-let soundPlaying = false
-let touchStates = ""
 let switchStates = ""
+let touchStates = ""
+let soundPlaying = false
+let slider2 = 0
+let slider1 = 0
+let spinner2 = 0
+let spinner1 = 0
+basic.showIcon(IconNames.No)
+RainbowSparkleUnicorn.Sound.setVolume(20)
+RainbowSparkleUnicorn.Sound.playTrack(1)
+basic.forever(function () {
+    RainbowSparkleUnicorn.Controls.dial1(randint(0, 254))
+    RainbowSparkleUnicorn.Controls.dial2(randint(0, 254))
+    basic.pause(1000)
+    basic.showIcon(IconNames.SmallHeart)
+})
 basic.forever(function () {
     spinner1 = RainbowSparkleUnicorn.Controls.encoder1value()
     spinner2 = RainbowSparkleUnicorn.Controls.encoder2value()
@@ -13,9 +22,10 @@ basic.forever(function () {
     soundPlaying = RainbowSparkleUnicorn.Sound.playingSound()
     touchStates = RainbowSparkleUnicorn.Touch.getTouchStates()
     switchStates = RainbowSparkleUnicorn.Switch.getSwitchStates()
-    basic.pause(50)
-})
-basic.forever(function () {
-    RainbowSparkleUnicorn.Controls.dial1(randint(0, 254))
-    basic.pause(1000)
+    RainbowSparkleUnicorn.Light.turnAllOn()
+    basic.showIcon(IconNames.Butterfly)
+    basic.pause(2000)
+    RainbowSparkleUnicorn.Light.turnAllOff()
+    basic.showIcon(IconNames.Meh)
+    basic.pause(2000)
 })
