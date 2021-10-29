817684fe39173ca074a4177bb4ffae6b9f2f3852
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2021-10-29 11:34:46 +0100

    Update pxt.json, dial.ts

---
+++ dial.ts
@@ -0,0 +1,29 @@
+
+namespace RainbowSparkleUnicorn.Dial {
+
+    export declare const enum Dials {
+        //% block="Dial 1"    
+        Dial1 = 0,
+        //% block="Dial 2"
+        Dial2 = 1
+    }
+
+    /**
+    * Set the analog dial to a certain voltage.
+    * @param voltage the touch sensor to be checked, eg: 15
+    */
+    //% subcategory="Sliders / Dials / Spinners" 
+    //% group="Dials"
+    //% block="Set dial %dial to $value"
+    //% value.min=0 value.max=255
+    //% weight=65
+    export function value(dial: Dials, value: number) {
+
+        if (dial == Dials.Dial1) {
+            _sendMessage("DIAL1," + Math.clamp(0, 255, value))
+        }
+        else {
+            _sendMessage("DIAL2," + Math.clamp(0, 255, value))
+        }
+    }
+}
