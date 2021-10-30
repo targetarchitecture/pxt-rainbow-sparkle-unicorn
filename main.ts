3ade0ee12d2169c1db4a8d560f4cfa46af11cb32
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2021-10-30 09:59:33 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 RainbowSparkleUnicorn.start()
 
-// RainbowSparkleUnicorn.Switch.onPressed(RainbowSparkleUnicorn.Switch.Pins.P0, function (pin) {
-//   basic.showNumber(pin)
-// })
+RainbowSparkleUnicorn.Switch.onPressed(RainbowSparkleUnicorn.Switch.Pins.P0, function () {
+  basic.showString("A")
+})
 
-// RainbowSparkleUnicorn.Switch.onPressed(RainbowSparkleUnicorn.Switch.Pins.P1,function (pin) {
-//     basic.showNumber(pin)
-// })
+RainbowSparkleUnicorn.Switch.onPressed(RainbowSparkleUnicorn.Switch.Pins.P1,function () {
+    basic.showString("B")
+})
 
 RainbowSparkleUnicorn.Switch.onReleased(RainbowSparkleUnicorn.Switch.Pins.P0, function () {
     basic.showIcon(IconNames.Heart)
@@ -18,4 +18,9 @@ RainbowSparkleUnicorn.Switch.onReleased(RainbowSparkleUnicorn.Switch.Pins.P1, fu
 
 RainbowSparkleUnicorn.Switch.onAnyPressed( function (pin) {
     basic.showNumber(pin)
+})
+
+input.onLogoEvent(TouchButtonEvent.Released, function() {
+    serial.redirectToUSB()
+    basic.showIcon(IconNames.Yes)
 })
