39819e1042745d7b516e5cc0aa7b4f45191aaf6b
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2022-07-30 17:03:37 +0100

    Update main.ts

--- main.ts
+++
@@ -1,8 +0,0 @@
-RainbowSparkleUnicorn.start()
-RainbowSparkleUnicorn.Light.turnAllOn()
-RainbowSparkleUnicorn.Sound.stop()
-basic.showIcon(IconNames.Heart)
-loops.everyInterval(1000, function () {
-    RainbowSparkleUnicorn.Sound.setVolume(4)
-    RainbowSparkleUnicorn.Sound.playTrack(Math.randomRange(1, 2))
-})
